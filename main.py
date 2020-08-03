db8d5f2b003243e0b25048d8d2272b12faea31a6
YashKhare143 <65073308+YashKhare143@users.noreply.github.com>
2020-08-03 18:21:27 +0530

    Add config.ini,update requorements.txt and modify main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-                             Welcome to Python-AI-1.0
+                             Welcome to Python-AI-2.0
 
 Kindly install all the Requirements from requirements.txt by using command - pip install -r requirements.txt
 You can do lots of things with it like sending emails, whatsapp messages,downloading youtube videos,play songs and much more
@@ -25,16 +25,29 @@ import socket
 from pytube import YouTube
 import pyshorteners
 import pywhatkit
+from configparser import ConfigParser
+import re 
+from selenium import webdriver
+
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+# browser = webdriver.Chrome('C:\\Users\\khare\\Downloads\\chromedriver_win32\\chromedriver.exe')
+
+#SettingUp config file
+file = 'config.ini'
+config = ConfigParser()
+config.read(file)
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[int(config['Voice']['type'])].id)
 # print(voices[2].id)
 rate = engine.getProperty('rate')   # getting details of current speaking rate
-# print (rate)                      #printing current voice rate
-engine.setProperty('rate', 190)     # setting up new voice rate
+
+engine.setProperty('rate', int(config['Voice']['rate']))     # setting up new voice rate
+# print (config['Voice']['rate'])                      #printing current voice rate
 chat = True                         #Toggle between chat and speechRecognition
-        
+
+
 
 def speak(audio):
     engine.say(audio)
@@ -50,8 +63,6 @@ def wishMe():
     
     else:
         speak("Good Evening Sir!")
-
-   
 
 def takeCommand():
     # it takes microphone input  from user
@@ -93,13 +104,161 @@ def takeCommand():
             return "None"
         return query
 
-def sendEmail(to,content):
+
+################################## Send Email ends here ##################################
+        #setup a SMTP server to send email
+def setupSMTPServer(mailAddress,message):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login('','')          #Type your gmail id and password here to send email
-    server.sendmail('kharey211@gmail.com',to,content)
+    server.login(config['Email']['yourMailId'],config['Email']['yourMailPass'])          #Type your gmail id and password here to send email
+    server.sendmail(config['Email']['yourMailId'],mailAddress,message)
     server.close()
+
+# query = query.replace("wikipedia","")
+        
+def sendEmail(name,mailAddress):
+    if chat == True:
+        try:
+            speak("please type a message: ")
+            message = input("=> ")
+            setupSMTPServer(mailAddress,message)
+            speak(f"Your message has been send to {name} with email address {mailAddress}.")
+        except Exception as e:
+            print(e)
+            speak("Sorry I am not abel to send email this time...")
+    elif chat == False:
+        try:
+            speak("Ok,what's the message")
+            message = takeCommand()
+            setupSMTPServer(mailAddress,message)
+            speak(f"Your message has been send to {name} with email address {mailAddress}.")
+        except Exception as e:
+            print(e)
+            speak("Sorry I am not abel to send email this time...")
+
+def addToMailList(name,mailAddress):
+    config.set('EmailAddress',name,mailAddress)
+    with open(file,'w') as configfile:
+        config.write(configfile)
+
+def askToAddMailToList(name,mailAddress):
+    if chat == True:
+        speak(f"Do you want me to add {name} Email address to your Email address list?")
+        addToList = input("please type yes or no:")
+        if addToList == "yes":
+            speak("Ok")
+            addToMailList(name,mailAddress)
+        elif addToList == "no":
+            speak("Ok sir as your wish.")
+            
+    elif chat == False:
+        speak(f"Do you want me to add {name} Email address to your Email address list?")
+        addToList = takeCommand()
+        if addToList == "yes":
+            speak("Ok")
+            addToMailList(name,mailAddress)
+        elif addToList == "no":
+            speak("Ok sir as your wish.")
+
+def mailValidition(name,mailAddress):
+    if(re.search(regex,mailAddress)):
+        if chat == True:
+            speak("Is it right mail?")
+            mailVerification = input("Pleas type yes or no: ")
+            
+            if mailVerification == "yes":
+                askToAddMailToList(name,mailAddress)
+                sendEmail(name,mailAddress)
+
+            elif mailVerification == "no":
+                emailVerification(name,mailAddress)
+
+        elif chat == False:
+            speak("Is it right mail?")
+            mailVerification = takeCommand()
+            
+            if mailVerification == "yes":
+                askToAddMailToList(name,mailAddress)
+                sendEmail(name,mailAddress)
+
+            elif mailVerification == "no":
+                emailVerification(name,mailAddress)
+
+    else:
+        speak("Invalid Email, Please enter the valide Email Address")
+        reEnterTheEmaill(name,mailAddress)
+
+def reEnterTheEmaill(name,mailAddress):
+    if chat == True:
+        mailAddress = input ("=> ")
+        mailValidition(name,mailAddress)
+    elif chat == False:
+        mailAddress = takeCommand()
+        mailAddress = mailAddress.replace(" ","")
+        mailValidition(name,mailAddress)
+    
+def emailVerification(name,mailAddress):
+    if chat == True:
+        speak(f"Please type the mail address of {name} again:")
+        mailAddress = input("=> ")
+        mailValidition(name,mailAddress)
+
+    elif chat == False:
+        speak(f"Please say the mail address of {name} again:")
+        mailAddress = takeCommand()
+        mailAddress = mailAddress.replace(" ","")
+        mailValidition(name,mailAddress)
+    
+def Email():
+    if chat == True:
+        try:
+            try:
+
+                speak("To whome you want me to send message?")
+                name = input("Please enter recipient name: ")
+                if config["EmailAddress"][name]:
+                    speak("this name is in your email address list")
+                    mailAddress = config["EmailAddress"][name]
+                    speak(f"the email address of {name} is {mailAddress}")
+                    sendEmail(name,mailAddress)
+            except Exception as e:
+                speak("this name is not in your email address list")
+                speak(f"What is the mail address of {name}: ")
+                mailAddress = input ("=> ")
+                mailValidition(name,mailAddress)
+                
+           
+        except Exception as e:
+            print(e)
+            speak("Sorry I am not abel to send email this time...")
+          
+    elif chat == False:
+        try:
+            try:
+
+                speak("To whome you want me to send message?")
+                name = takeCommand()
+                if config["EmailAddress"][name]:
+                    speak("this name is in your email address list")
+                    mailAddress = config["EmailAddress"][name]
+                    speak(f"the email address of {name} is {mailAddress}")
+                    sendEmail(name,mailAddress)
+            except Exception as e:
+                speak("this name is not in your email address list")
+                speak(f"What is the mail address of {name}: ")
+                mailAddress = takeCommand()
+                mailAddress = mailAddress.replace(" ","")
+                mailValidition(name,mailAddress)
+                
+        except Exception as e:
+            print(e)
+            speak("Sorry I am not abel to send email this time...")
+
+        
+
+###################################### Send Email ends here #################################
+
 
 #To find your system Ip Address
 def ipFiender():
@@ -109,17 +268,29 @@ def ipFiender():
     print(f"Your IP Address is: {ip_address}")
     speak(f"Your IP Address is {ip_address}")
 
+###################################### To Send WhatsApp Message ###################################### 
 def wpMsgSender():
     strTimeH = datetime.datetime.now().strftime("%H")
     strTimeM = datetime.datetime.now().strftime("%M")
-    speak("to whome you want me to send message?")
-    name = input("Please enter name: ")
-    number = str(input("Please enter number with country code: "))
-    speak("Ok, what's the message?")
-    message = input("Please type a message")
-    pywhatkit.sendwhatmsg(number,message,int(strTimeH),int(strTimeM) + 2)
-    speak(f"message has been send to {name}")
+    if chat == True:
+        speak("to whome you want me to send message?")
+        name = input("Please enter name: ")
+        number = str(input("Please enter number with country code: "))
+        speak("Ok, what's the message?")
+        message = input("Please type a message")
+        pywhatkit.sendwhatmsg(number,message,int(strTimeH),int(strTimeM) + 2)
+        speak(f"message has been send to {name}")
 
+    elif chat == False:
+        speak("to whome you want me to send message?")
+        name = takeCommand()
+        speak("Please say number with country code: ")
+        number = takeCommand()
+        speak("Ok, what's the message?")
+        message = takeCommand()
+        pywhatkit.sendwhatmsg(number,message,int(strTimeH),int(strTimeM) + 2)
+        speak(f"message has been send to {name}")
+############################ To Send WhatsApp Message end's here ############################ 
 
 #YouTube video downloader
 def YTVdownloader():
@@ -180,6 +351,19 @@ if __name__ == "__main__":
 
         elif "open youtube" in query:
             webbrowser.open("youtube.com")
+            # browser.get('http://youtube.com/')
+
+        elif "open google" in query:
+            webbrowser.open("google.com")
+            # browser.get('http://google.com/')
+
+        elif "open instagram" in query:
+            webbrowser.open("instagram.com")
+            # browser.get('https://www.instagram.com//')
+
+        elif "open github" in query:
+            webbrowser.open("github.com")
+            # browser.get('https://github.com//')
 
         elif "open chrome" in query:
             os.startfile("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
@@ -220,16 +404,8 @@ if __name__ == "__main__":
             codePath = "C:\\Users\\khare\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
-        elif 'email to yash' in query:
-            try:
-                speak("what should I say?")
-                content = takeCommand()
-                to = "yashkhare383@gmail.com"
-                sendEmail(to,content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry I am not abel to send email this time...")
+        elif 'email' in query:
+            Email()
 
         elif "ip address" in query:
             ipFiender()
@@ -243,9 +419,7 @@ if __name__ == "__main__":
         elif 'i want to type a command for you' in query:
             chat = True
            
-        elif "exit" or "stop" in query:
-            speak("Bye")
-            break
+        
     # pass
   
     
